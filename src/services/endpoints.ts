e8110c9c3ecaf57b2b0b4a4a3a4d21c0d8da3d86
lbl <long.lebao2008@gmail.com>
2025-08-26 00:27:37 +0700

    load list product

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -4,8 +4,8 @@ export const endpoints = {
     login: "/auth/admin/login".trim(),
 
     /**region Product */
-    pagination: "/api/product/search",
-    product_create: "/api/product/create",
-    product_update: (id: string) => `/api/product/update/${id}`
+    pagination: "/products/search",
+    product_create: "/products/create",
+    product_update: (id: string) => `/products/update/${id}`
     /**endregion */
 }
